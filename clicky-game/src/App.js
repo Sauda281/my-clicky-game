cec7d44bb9e47c9e285b5de4c15ca004c2ca3be1
Sauda281 <58537112+Sauda281@users.noreply.github.com>
2020-05-31 13:40:43 -0500

    styling

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Card from "./components/Cards";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import flowers from "./cards.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component{
+  state = {
+    flowers,
+    clickedFlowerIds: [],
+    score: 0,
+    goal: 8,
+    status: ""
+  };
+
+  shuffleScoreCard = id => {
+    let clickedFlowerIds = this.state.clickedFlowerIds;
+
+    if(clickedFlowerIds.includes(id)){
+      this.setState({ clickedFlowerIds: [], score: 0, status: "You lost! Click to play again"});
+      return;
+    }else{
+      clickedFlowerIds.push(id)
+
+      if(clickedFlowerIds.length === 8){
+        this.setState({score: 8, status: "Congratulations, you won!. Click to play again!", clickedFlowerIds: []});
+        console.log('You Win');
+        return;
+      }
+
+      this.setState({ flowers, clickedFlowerIds, score: clickedFlowerIds.length, status: " "});
+
+      for (let i = flowers.length - 1; i > 0; i--) {
+        let k = Math.floor(Math.random() * (i + 1));
+        [flowers[i], flowers[k]] = [flowers[k], flowers[i]];
+      }
+    }
+  }
+
+  // render a card component for each card object
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1 className="App-title">Clicky Game</h1>
+        <p className="App-intro">Try not to click the same image twice!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </header>
+        <Score total={this.state.score}
+        goal={8}
+        status={this.state.status} />
+
+        <Wrapper>
+          {this.state.flowers.map(flower =>(
+            <Card
+            shuffleScoreCard={this.shuffleScoreCard}
+            id={flower.id}
+            key={flower.id}
+            image={flower.image}
+            />
+          ))}
+        </Wrapper>
+        <footer>
+
+        </footer>
+      </div>
+    );
+  }
 }
+
 
 export default App;
